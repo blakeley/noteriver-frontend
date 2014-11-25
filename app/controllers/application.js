74244de23b011e95223b5fb7e31dd7612d85aec8
Brandon Blakeley <brandon.blakeley@gmail.com>
2014-11-24 19:08:50 -0800

    Refactor session into separate controller

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,41 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  init: function() {
-    this.set("email", "");
-    this.set("password", "");
-    this.set("token", localStorage.getItem("token"));
-  },
-
-
-  tokenChanged: function(){
-    if(this.get('token')){
-      localStorage["token"] = this.get('token');
-    } else {
-      localStorage.removeItem('token');
-    }
-  }.observes('token'),
-
-  actions: {
-    register: function() {
-      var account = {"user": {"email": this.get('email'), "password": this.get('password')}};
-      var _this = this;
-      Ember.$.post('api/v1/users', account).then(
-        function(response){
-          Ember.run(function(){
-            _this.set('token', response.session.token);            
-          });
-        },
-        function(response){
-          Ember.run(function(){
-            _this.set("errorMessages", response.responseJSON.errors);
-          });
-        }
-      );  
-    },
-
-    logout: function() {
-      this.set('token', null);
-    }
-  }
 });
