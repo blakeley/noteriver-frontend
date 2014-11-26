ae35614002832bcb5a88fe9043ecc7c9894d4e5d
Brandon Blakeley <brandon.blakeley@gmail.com>
2014-11-25 20:51:56 -0800

    Improve register/login modal styling

--- app/controllers/log-in.js
+++ app/controllers/log-in.js
@@ -4,25 +4,20 @@ export default Ember.Controller.extend({
   needs: "session",
   
   init: function() {
-    this.set("email", "");
-    this.set("password", "");
-    this.set("open", false);
+    this.send('clear');
   },
 
   token: Ember.computed.alias('controllers.session.token'),
 
   actions: {
-    login: function() {
+    create: function() {
       var account = {"user": {"email": this.get('email'), "password": this.get('password')}};
       var _this = this;
       Ember.$.post('api/v1/sessions', account).then(
         function(response){
           Ember.run(function(){
             _this.set('token', response.session.token);
-            _this.set('errorMessages', null);
-            _this.set("email", "");
-            _this.set("password", "");
-            _this.set("open", false);
+            _this.send('clear');
           });
         },
         function(response){
@@ -32,8 +27,17 @@ export default Ember.Controller.extend({
         }
       );  
     },
-    toggleOpen: function() {
-      this.set('open', !this.get('open'));
-    }
+    open: function() {
+      this.set('open', true);
+    },
+    close: function() {
+      this.set('open', false);
+    },
+    clear: function() {
+      this.set('errorMessages', null);
+      this.set('email', '');
+      this.set('password', '');
+      this.set('open', false);
+    },
   }
 });
