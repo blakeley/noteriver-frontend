779a6e65663100d40fe3b94f96bc3a51709b33ba
Brandon Blakeley <brandon.blakeley@gmail.com>
2014-12-01 17:45:57 -0800

    Add Score model, route, controller, index template

---
+++ server/mocks/scores.js
@@ -0,0 +1,12 @@
+module.exports = function(app) {
+  var express = require('express');
+  var scoresRouter = express.Router();
+  scoresRouter.get('/', function(req, res) {
+    res.send({"scores":[
+      {"id": 1, "title": "Indiana Jones", "artist": "John Williams", "file_url": "https://s3.amazonaws.com/noteriver2/jones.mid"},
+      {"id": 2, "title": "Symphony 40", "artist": "Mozart", "file_url": "https://s3.amazonaws.com/noteriver2/jones.mid"},
+      {"id": 3, "title": "Moonlight Sonata", "artist": "Beethoven", "file_url": "https://s3.amazonaws.com/noteriver2/jones.mid"},
+      ]});
+  });
+  app.use('/api/v1/scores', scoresRouter);
+};
