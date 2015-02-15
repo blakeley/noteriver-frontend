2b17eb26fecb8895696bc51829999b345d63b265
Brandon Blakeley <brandon.blakeley@gmail.com>
2015-02-15 15:03:09 -0800

    Add isEbony property to piano-roll-note

--- app/components/piano-roll-note.js
+++ app/components/piano-roll-note.js
@@ -1,27 +1,37 @@
 import Ember from 'ember';
 
+var EBONIES = [1,3,6,8,10];
+
 export default Ember.Component.extend({
   tagName: 'rect',
   attributeBindings: ['x','y','rx','ry','width','height','fill','stroke'],
 
   x: function(){
-    return this.get('note.pitch') * 10 - 400;
+    return (this.get('note.pitch') - 36) * 20;
   }.property(),
 
   y: function(){
-    return this.get('note.onSecond') * 40;
+    return this.get('note.onSecond') * 200;
   }.property(),
 
   rx: 3,
   ry: 3,
-  width: 10,
+  width: 20,
 
   height: function(){
-    return (this.note.offSecond - this.note.onSecond) * 20;
+    return (this.note.offSecond - this.note.onSecond) * 200;
   }.property(),
 
   fill: function(){
-    return '#96B8D9';
+    if(this.get('isEbony')){
+      return '#4B76AC';
+    } else {
+      return '#96B8D9';
+    }
+  }.property(),
+
+  isEbony: function(){
+    return EBONIES.indexOf(this.get('note.pitch') % 12) > -1;
   }.property(),
 
   stroke: '#5E5E5E',
--- server/mocks/scores.js
+++ server/mocks/scores.js
@@ -1,15 +1,16 @@
+var SCORES = [
+  {"id": 1, "title": "Indiana Jones", "artist": "John Williams", "file_url": "https://s3.amazonaws.com/noteriver2/jones.mid"},
+  {"id": 2, "title": "chromatic Scale", "artist": "George P. Burdell", "file_url": "https://s3.amazonaws.com/noteriver2/chromatic.mid"},
+]
+
 module.exports = function(app) {
   var express = require('express');
   var scoresRouter = express.Router();
   scoresRouter.get('/', function(req, res) {
-    res.send({"scores":[
-      {"id": 1, "title": "Indiana Jones", "artist": "John Williams", "file_url": "https://s3.amazonaws.com/noteriver2/jones.mid"},
-      {"id": 2, "title": "Symphony 40", "artist": "Mozart", "file_url": "https://s3.amazonaws.com/noteriver2/jones.mid"},
-      {"id": 3, "title": "Moonlight Sonata", "artist": "Beethoven", "file_url": "https://s3.amazonaws.com/noteriver2/jones.mid"},
-      ]});
+    res.send({"scores": SCORES});
   });
   scoresRouter.get('/:id', function(req, res) {
-    res.send({"score": {"id": 1, "title": "Indiana Jones", "artist": "John Williams", "file_url": "https://s3.amazonaws.com/noteriver2/jones.mid"}});
+    res.send({"score": SCORES[req.params.id - 1]});
   });
   app.use('/api/v1/scores', scoresRouter);
 };
