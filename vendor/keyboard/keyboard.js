43e30c6b926ab7880fa7a72ad60dfddd78aea13d
Brandon Blakeley <brandon.blakeley@gmail.com>
2015-02-24 15:53:50 -0800

    Add keyboard.x and keyboard.width

--- vendor/keyboard/keyboard.js
+++ vendor/keyboard/keyboard.js
@@ -1,6 +1,9 @@
 
 
 var keyboard = {
+  IVORY_WIDTH: 24,
+  EBONY_WIDTH: 14,
+  X_OFFSETS: [0, 15, 24, 44, 48, 72, 85, 96, 113, 120, 141, 144],
   note: function(pitch) {
     return {
       isEbony: function(){
@@ -8,7 +11,17 @@ var keyboard = {
       },
       isIvory: function(){
         return ![1,3,6,8,10].contains(pitch % 12);
+      },
+      width: function(){
+        if(this.isIvory()){
+          return keyboard.IVORY_WIDTH;
+        } else {
+          return keyboard.EBONY_WIDTH;
+        }
+      },
+      x: function(){
+        return Math.floor(pitch / 12) * keyboard.IVORY_WIDTH * 7 + keyboard.X_OFFSETS[pitch % 12];
       }
-    }    
+    }
   }
 }
