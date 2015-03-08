d7b61e2676d42daf17e95214cdaa74a4badcb9a3
Brandon Blakeley <brandon.blakeley@gmail.com>
2015-03-08 12:35:59 -0700

    Add settings-panel#isOpen toggle button

--- app/components/settings-panel.js
+++ app/components/settings-panel.js
@@ -1,5 +1,12 @@
-import Ember from 'ember';
+;import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['settings-panel-container'],
+  classNameBindings: ['isOpen'],
+  isOpen: true,
+  actions: {
+    toggleIsOpen: function(){
+      this.toggleProperty('isOpen');
+    }
+  },
 });
