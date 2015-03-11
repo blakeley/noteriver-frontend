a0e1c9953aad48f0c03777788049ddd9a9f115cc
Brandon Blakeley <brandon.blakeley@gmail.com>
2015-03-11 14:24:27 -0700

    Add controller:application.logout()

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
   storage: Ember.inject.service(),
 
+  actions: {
+    logout: function() {
+      this.set('model.authToken', null);
+      this.set('model.user', null);
+      this.get('storage').removeItem('authToken');
+      this.get('storage').removeItem('currentUserId');
+    },
+  },
 
 });
--- tests/unit/application/controller-test.js
+++ tests/unit/application/controller-test.js
@@ -1,3 +1,5 @@
+/* global Ember */
+
 import {
   moduleFor,
   test
@@ -5,11 +7,78 @@ import {
 
 moduleFor('controller:application', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['service:storage'],
 });
+
+var mockStorage = {
+  init: function(){
+    this.hash = {};
+  },
+  getItem: function(key) {
+    return this.hash[key];
+  },
+  setItem: function(key, value) {
+    this.hash[key] = value;
+  },
+  removeItem: function(key) {
+    delete this.hash[key];
+  },
+};
 
 // Replace this with your real tests.
 test('it exists', function(assert) {
   var controller = this.subject();
   assert.ok(controller);
 });
+
+test('it uses service:storage', function(assert){
+  var controller = this.subject();
+  assert.ok(controller.get('storage'));
+});
+
+test('.logout() clears the authToken', function(assert) {
+  var controller = this.subject();
+  controller.set('model', Ember.Object.create({
+    authToken: 'token',
+  }));
+  controller.send('logout');
+  assert.ok(!controller.get('model.authToken'));
+});
+
+test('.logout() disassociates the user', function(assert) {
+  var controller = this.subject();
+  controller.set('model', Ember.Object.create({
+    user: Ember.Object.create(),
+  }));
+  controller.send('logout');
+  assert.ok(!controller.get('model.user'));  
+});
+
+test('.logout() clears the authToken in localStorage', function(assert) {
+  mockStorage.init();
+  mockStorage.setItem('authToken','token');
+  var controller = this.subject({storage: mockStorage});
+  controller.set('model', Ember.Object.create());
+  controller.send('logout');
+  assert.ok(!mockStorage.getItem('authToken'));
+});
+
+test('.logout() clears the currentUserId in localStorage', function(assert) {
+  mockStorage.init();
+  mockStorage.setItem('currentUserId', 1337);
+  var controller = this.subject({storage: mockStorage});
+  controller.set('model', Ember.Object.create());
+  controller.send('logout');
+  assert.ok(!mockStorage.getItem('currentUserId'));
+});
+
+
+
+
+
+
+
+
+
+
+
