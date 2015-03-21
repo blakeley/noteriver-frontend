a5f1c385096c0507c1a8eb5ef51c19cedcb1b33b
Brandon Blakeley <brandon.blakeley@gmail.com>
2015-03-20 18:02:59 -0700

    Add midi-player#isPlaying with control-bar UI

--- app/components/midi-player/component.js
+++ app/components/midi-player/component.js
@@ -2,27 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['midi-player'],
-  time: 1,
-  settingsPanelIsOpen: true,
 
+  time: 1,
+  isPlaying: true,
+  settingsPanelIsOpen: true,
   lowNumber: 0,
   highNumber: 88,
+
+  boundLowNumber: function(){
+    if(parseInt(this.get('lowNumber')) >= parseInt(this.get('highNumber'))){
+      this.set('highNumber', parseInt(this.get('lowNumber')) + 1);
+    }
+  }.observes('lowNumber'),
+
+  boundHighNumber: function(){
+    if(parseInt(this.get('lowNumber')) >= parseInt(this.get('highNumber'))){
+      this.set('lowNumber', parseInt(this.get('highNumber')) - 1);
+    }
+  }.observes('highNumber'),
+
   actions: {
     toggleSettingsPanelIsOpen: function(){
       this.toggleProperty('settingsPanelIsOpen');
     },
   },
 
-  lowNumberChanged: function(){
-    if(parseInt(this.get('lowNumber')) >= parseInt(this.get('highNumber'))){
-      this.set('highNumber', parseInt(this.get('lowNumber')) + 1);
-    }
-  }.observes('lowNumber'),
-
-  highNumberChanged: function(){
-    if(parseInt(this.get('lowNumber')) >= parseInt(this.get('highNumber'))){
-      this.set('lowNumber', parseInt(this.get('highNumber')) - 1);
-    }
-  }.observes('highNumber'),
 
 });
