bf40dee53882bf3e9e4cecba65b2dd29de61dfb7
Brandon Blakeley <brandon.blakeley@gmail.com>
2015-03-21 16:45:40 -0700

    Add midi-player.play

--- app/components/midi-player/component.js
+++ app/components/midi-player/component.js
@@ -4,18 +4,34 @@ export default Ember.Component.extend({
   classNames: ['midi-player'],
 
   time: 1,
-  isPlaying: true,
+  isPlaying: false,
   settingsPanelIsOpen: true,
   lowNumber: 0,
   highNumber: 88,
 
-  boundLowNumber: function(){
+  play: function(){
+    var component = this;
+    var lastAnimateTime = Date.now();
+    function animate(){
+      if(component.get('isPlaying')){
+        var now = Date.now();
+        var deltaTime = (now - lastAnimateTime) / 1000;
+        lastAnimateTime = now;
+        console.log(deltaTime);
+        component.set('time', parseFloat(component.get('time')) + deltaTime);
+        window.requestAnimationFrame(animate);
+      }
+    }
+    window.requestAnimationFrame(animate);
+  }.observes('isPlaying'),
+
+  boundHighNumber: function(){
     if(parseInt(this.get('lowNumber')) >= parseInt(this.get('highNumber'))){
       this.set('highNumber', parseInt(this.get('lowNumber')) + 1);
     }
   }.observes('lowNumber'),
 
-  boundHighNumber: function(){
+  boundLowNumber: function(){
     if(parseInt(this.get('lowNumber')) >= parseInt(this.get('highNumber'))){
       this.set('lowNumber', parseInt(this.get('highNumber')) - 1);
     }
