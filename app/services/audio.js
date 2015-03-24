f4627c0c007df9a61f1e50124d65bebbae89b30f
Brandon Blakeley <brandon.blakeley@gmail.com>
2015-03-23 20:57:12 -0700

    Add service:audio#totalLoaded and service:audio#percentLoaded

--- app/services/audio.js
+++ app/services/audio.js
@@ -5,13 +5,25 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   context: new AudioContext(),
   buffers: new Ember.Map(),
+  buffersA: Ember.A([]), // https://github.com/emberjs/ember.js/issues/10209
+
+  totalLoaded: 0,
+  percentLoaded: function(){
+    if(this.get('buffersA.length') < 1){
+      return 0;
+    } else {
+      return this.get('totalLoaded') / this.get('buffersA.length');
+    }
+  }.property('buffersA.length', 'totalLoaded'),
 
   getBuffer: function(url){
     var context = this.get('context');
     var buffers = this.get('buffers');
+    var buffersA = this.get('buffersA');
+    var _this = this;
     if(buffers.has(url)){
       return buffers.get(url);
-    }
+    } 
     var buffer = new Ember.RSVP.Promise(function(resolve, reject){
       var xhr = new XMLHttpRequest();
       xhr.open('GET', url);
@@ -29,16 +41,25 @@ export default Ember.Service.extend({
     }).then(function(audioData){
       return new Ember.RSVP.Promise(function(resolve/*, reject*/){
         context.decodeAudioData(audioData, function(buffer){
+          _this.incrementProperty('totalLoaded');
           resolve(buffer);
         });
       });
     });
 
     buffers.set(url, buffer);
+    buffersA.pushObject(buffer);
+
     return buffer;
   },
-
-
-
-
 });
+
+
+
+
+
+
+
+
+
+
--- tests/unit/services/audio-test.js
+++ tests/unit/services/audio-test.js
@@ -1,5 +1,6 @@
 /* global AudioContext, AudioBuffer */
 
+import Ember from 'ember';
 import {
   moduleFor,
   test
@@ -10,6 +11,7 @@ moduleFor('service:audio', {
   // needs: ['service:foo']
   setup: function(){
     this.subject().get('buffers').clear();
+    this.subject().get('buffersA').clear();
   },
 });
 
@@ -49,22 +51,59 @@ test('#getBuffer resolves to an audioBuffer', function(assert) {
 
 test('#getBuffer memoizes the buffer (promise) for future calls', function(assert) {
   var service = this.subject();
-  assert.equal(service.buffers.length, 0);
+  assert.equal(service.buffers.size, 0);
   return service.getBuffer(url).then(function(buffer){
-    console.log("made it inside");
-    assert.equal(service.buffers.length, 1);
-    console.log("didn't make it here");
+    assert.equal(service.buffers.size, 1);
   });
 });
 
 test('successive calls to #getBuffer with the same url returns the same buffer (promise)', function(assert) {
+  expect(2);
   var service = this.subject();
   var first = service.getBuffer(url);
   return first.then(function(buffer){
     var second = service.getBuffer(url);
     assert.equal(first, second);
+    return second.then(function(buffer){
+      assert.ok(true, "can resolve again");
+    });
   });
 });
+
+test('#totalLoaded returns the number of buffers that have loaded', function(assert) {
+  var service = this.subject();
+  var promise = service.getBuffer(url);
+  assert.equal(service.get('totalLoaded'), 0);
+  return promise.then(function(buffer){
+    assert.equal(service.get('totalLoaded'), 1);
+  });
+});
+
+test('#percentLoaded returns the percent of buffers that have loaded', function(assert) {
+  var service = this.subject();
+  var resolved = Ember.RSVP.resolve();
+  var resolved2 = Ember.RSVP.resolve();
+  var unresolved = new Ember.RSVP.Promise(function(){});
+  assert.equal(service.get('percentLoaded'), 0);
+  service.buffersA.pushObject(resolved);
+  service.incrementProperty('totalLoaded');
+  assert.equal(service.get('percentLoaded'), 1);
+  service.buffersA.pushObject(unresolved);
+  assert.equal(service.get('percentLoaded'), 0.5);
+  var promise = service.getBuffer(url);
+  assert.equal(service.get('percentLoaded'), 1/3);
+  return promise.then(function(buffer){
+    assert.equal(service.get('percentLoaded'), 2/3);
+  });
+});
+
+
+
+
+
+
+
+
 
 
 
