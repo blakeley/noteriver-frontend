88c87832ac9a628cddcb1bb7eed5637b32c7e2c6
Brandon Blakeley <brandon.blakeley@gmail.com>
2015-11-10 19:31:38 -0800

    Include AUTHORIZATION header in application adapter

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,5 +1,13 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
+  session: Ember.inject.service(),
+
   namespace: 'api/v1',
+  headers: Ember.computed('session.authToken', function(){
+    return {
+      'AUTHORIZATION': this.get('session.authToken'),
+    };
+  }),
+
 });
