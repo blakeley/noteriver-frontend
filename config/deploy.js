a80c833d3fdab53209bc49bac3d7f29220ddd885
Brandon Blakeley <brandon.blakeley@gmail.com>
2016-02-04 20:42:06 -0800

    Fix deploy

--- config/deploy.js
+++ config/deploy.js
@@ -1,25 +1,25 @@
 var VALID_DEPLOY_TARGETS = [ //update these to match what you call your deployment targets
-  'prod'
+  'prod',
+  'production',
 ];
 
 module.exports = function(deployTarget) {
   var ENV = {
     build: {
-      environment: 'production'
+      environment: 'production',
     },
     redis: {
+      url: process.env.NOTERIVER_REDIS_URL,
+      keyPrefix: 'noteriver:index',
       allowOverwrite: true,
-      keyPrefix: 'noteriver:index'
     },
     s3: {
       prefix: '',
       accessKeyId: process.env.NOTERIVER_AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.NOTERIVER_AWS_SECRET_ACCESS_KEY,
       bucket: 'noteriver',
+      region: 'us-east-1',
     },
-    redis: {
-      url: process.env.NOTERIVER_REDIS_URL
-    }
   };
 
   if (VALID_DEPLOY_TARGETS.indexOf(deployTarget) === -1) {
